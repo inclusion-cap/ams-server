9cf0c30a922eab315d166b79ac7219a8072bc6cc
Shahid <shahid.sarker@gmail.com>
2019-08-13 20:12:58 -0400

    admin attempt

--- app.js
+++ app.js
@@ -14,10 +14,58 @@ const Campaign = db.Campaign;
 const Submission = db.Submission;
 const Comment = db.Comment;
 
+const passport = require("passport");
+const Strategy = require("passport-local").Strategy;
+
+let demoAdmin = { username: "foo@example.com", password: "barbaz" };
+Admin.create(demoAdmin);
+Admin.findOne()
+  .then(user => (demoAdmin = user))
+  .catch(err => Admin.create(demoAdmin));
+
+passport.use(
+  new Strategy(function(username, password, cb) {
+    Admin.findOne({ username: username }, function(err, admin) {
+      if (err) {
+        return cb(err);
+      }
+      if (!admin) {
+        return cb(null, false);
+      }
+      // if (!admin.verifyPassword(password)) {
+      if (admin.password != password) {
+        return cb(null, false);
+      }
+      return db(null, admin);
+    });
+  })
+);
+
+passport.serializeUser(function(admin, cb) {
+  cb(null, admin.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  Admin.findOne({ where: { id: id } })
+    .then(admin => {
+      cb(null, admin);
+    })
+    .catch(err => cb(err));
+});
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+
+// passport setup demo login POST route
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/login" }),
+  function(req, res) {
+    res.redirect("/");
+  }
+);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -28,6 +76,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const passport = require("passport");
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// passport setup demo index GET route
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express", user: req.user });
 });
+
+// passport setup demo login GET route
+router.get("/login", function(req, res, next) {
+  res.render("login");
+});
+
+// // passport setup demo login POST route
+// router.post(
+//   "/login",
+//   passport.authenticate("local", { failureRedirect: "/login" }),
+//   function(req, res) {
+//     res.redirect("/");
+//   }
+// );
+
+// passport setup demo logout GET route
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+// passport setup demo GET route
+router.get(
+  "/profile",
+  require("connect-ensure-login").ensureLoggedIn(),
+  function(req, res) {
+    res.render("profile", { user: req.user });
+  }
+);
 
 module.exports = router;
