c8dbef237008b533bb5e856d3126f4574e3a0485
Shahid <shahid.sarker@gmail.com>
2019-08-16 17:30:47 -0400

    routes

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ var router = express.Router();
 const models = require("../models");
 
 const Campaign = models.Campaign;
+const Admin = models.Campaign;
+const Comment = models.Comment;
+const Submission = models.Submission;
 
 const passport = require("passport");
+const ensureLoggedIn = require("connect-ensure-login").ensureLoggedIn;
 
 // passport setup demo index GET route
 router.get("/", function(req, res, next) {
@@ -42,29 +46,32 @@ router.get("/logout", function(req, res) {
 // );
 
 /* GET home page. */
-router.get(
-  "/campaigns",
-  require("connect-ensure-login").ensureLoggedIn(),
-  function(req, res, next) {
-    Campaign.findAll()
-      .then(campaigns => {
-        res.status(200).json(campaigns);
-      })
-      .catch(err => err);
-  }
-);
+router.get("/campaigns", ensureLoggedIn("/login"), function(req, res, next) {
+  Campaign.findAll()
+    .then(campaigns => {
+      res.status(200).json(campaigns);
+    })
+    .catch(err => err);
+});
 
 router.post("/campaigns", async (req, res) => {
   // if logged in
   var name = req.body.name;
   var fields = JSON.stringify(req.body.fields);
-  const campaign = await models.Campaign.create({ name: name, fields: fields });
+  const campaign = await Campaign.create({ name: name, fields: fields });
   res.status(201).json(campaign);
 });
 
-router.get("/campaigns/:campaignId", async (req, res) => {
-  var data = await models.Campaign.findByPk(req.params.campaignId);
-  res.status(200).json(data);
+router.get("/campaigns/:campaignId", ensureLoggedIn("/login"), function(
+  req,
+  res,
+  next
+) {
+  Campaign.findByPk(req.params.campaignId)
+    .then(campaign => {
+      res.status(200).json(campaign);
+    })
+    .catch(err => err);
 });
 
 router.post("/admins", async (req, res) => {
@@ -75,16 +82,34 @@ router.post("/admins", async (req, res) => {
   res.status(201).json(admin);
 });
 
-router.get("campaigns/:campaignId/submissions", async (req, res) => {
-  // if logged in
-  var campaign = await models.Campaign.findByPk(req.params.campaignId);
-  var submissions = campaign.getSubmissions();
-  res.status(200).json(submissions);
-});
+router.get(
+  "campaigns/:campaignId/submissions",
+  ensureLoggedIn("/login"),
+  function(req, res, next) {
+    // if logged in
+
+    const campaignId = req.params.campaignId;
+    // new
+    Campaign.findByPk(campaignId)
+      .then(campaign => {
+        campaign
+          .getSubmissions()
+          .then(submissions => {
+            res.status(200).json(submissions);
+          })
+          .catch(err => err);
+      })
+      .catch(err => err);
+    // old
+    // var campaign = Campaign.findByPk(req.params.campaignId);
+    // var submissions = campaign.getSubmissions();
+    // res.status(200).json(submissions);
+  }
+);
 
 router.post("campaigns/:campaignId/submissions", async (req, res) => {
   // if logged in
-  var campaign = await models.Campaign.findByPk(req.params.campaignId);
+  var campaign = await Campaign.findByPk(req.params.campaignId);
 
   var email = req.body.email;
   var content = JSON.stringify(req.body.content);
